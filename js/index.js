4cdc647b74bf5e76aa59828c12476693dbcba7b5
gidgenf <gidgenf@hotmail.com>
2023-08-22 10:13:20 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 const guardar = document.getElementById('buttonText')
 const texto = document.getElementById('inputText').value
-const data = document.getElementById('data')
 
 localStorage.setItem('parrafo')
 
 guardar.addEventListener('click', function() {
    const local = localStorage.getItem('parrafo')
 
-   data.innerHTML += parrafo
+   texto.innerHTML += local
 })
